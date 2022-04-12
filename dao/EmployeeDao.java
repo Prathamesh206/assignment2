469dcbfdd4c84d8a3d5984c05282d1f4d6ae086e
Prathamesh206 <98940544+Prathamesh206@users.noreply.github.com>
2022-04-12 17:17:49 +0530

    Update EmployeeDao.java

--- dao/EmployeeDao.java
+++ dao/EmployeeDao.java
@@ -21,7 +21,10 @@ public class EmployeeDao {
 	final int JOB=4;
 	final int EXISTING_FIRSTNAME=5;
 	final int EXISTING_LASTNAME=6;
-
+	final int UPDATE_CITY=1;
+	final int UPDATE_JOB=2;
+	final int UPDATE_FIRST_NAME=3;
+	final int UPDATE_LAST_NAME=4;
 
 	Connection connection=DBConnection.getConnection();
 
@@ -30,11 +33,23 @@ public class EmployeeDao {
 	 * 
 	 * insertEmployee method for storing the employee in the database
 	 * 
+	 * 
 	 */
 
-	public void insert(ArrayList<Employee> jsonEmployeeList) {
+
+	@SuppressWarnings("resource")
+	public void upsert(ArrayList<Employee> jsonEmployeeList) {
+		
 		EducationDao educationDao=new EducationDao();
+		PreparedStatement preparedStatement=null;
+		ArrayList<String> databaseEducations=new ArrayList<String>();
 		int empId=0;
+		String databaseJob=null;
+		String databaseCity=null;
+		boolean update=true;
+		
+		//   for Insert data into employee_data & eduaction_data if the firstName and lastName not exist in database
+		
 		String insertquery="INSERT INTO employee_data(firstname,lastname,city,job) SELECT * FROM (SELECT ? AS firstname, ? AS"        //insert query for insert the employee in the database if the employee is not present in the database
 				+ " lastname,? AS city,? AS job) AS temp WHERE NOT EXISTS (SELECT * FROM  employee_data WHERE firstname = ? "
 				+ "and lastname=?)  ";
@@ -43,58 +58,179 @@ public class EmployeeDao {
 			connection.setAutoCommit(false);                                                                //set AutoCommit false
 			for(Employee employee:jsonEmployeeList) {                                                      //iterating employee from jsonEmployee List
 
-				PreparedStatement employeeStatment=connection.prepareStatement(insertquery);            
-				employeeStatment.setString(FIRSTNAME, employee.getFirstName());
-				employeeStatment.setString(LASTNAME, employee.getLastName());
-				employeeStatment.setString(CITY, employee.getCity());
-				employeeStatment.setString(JOB, employee.getJob());
-				employeeStatment.setString(EXISTING_FIRSTNAME, employee.getFirstName());
-				employeeStatment.setString(EXISTING_LASTNAME, employee.getLastName());
-				int rowInserted=employeeStatment.executeUpdate();
+				preparedStatement=connection.prepareStatement(insertquery);            
+				preparedStatement.setString(FIRSTNAME, employee.getFirstName());
+				preparedStatement.setString(LASTNAME, employee.getLastName());
+				preparedStatement.setString(CITY, employee.getCity());
+				preparedStatement.setString(JOB, employee.getJob());
+				preparedStatement.setString(EXISTING_FIRSTNAME, employee.getFirstName());
+				preparedStatement.setString(EXISTING_LASTNAME,employee.getLastName());
+				int rowInserted=preparedStatement.executeUpdate();
+
+				//fetch the id from the database and pass to the educationDao insert method
+				ArrayList<Employee> dataBaseEmployeList=getEmployeeList(employee.getFirstName(),employee.getLastName());
+				for(Employee databaseEmployee:dataBaseEmployeList) {
+					empId=databaseEmployee.getId();
+					databaseCity=databaseEmployee.getCity();
+					databaseJob=databaseEmployee.getJob();
+				}
+
+
 				
-				if(rowInserted>0) {                                                                      
-					String selectQuery="select * from employee_data where firstname=? and lastname=?";       //select query for fetch the record from the database
-					PreparedStatement preparedStatement=connection.prepareStatement(selectQuery); 
-					preparedStatement.setString(FIRSTNAME, employee.getFirstName());                   
-					preparedStatement.setString(LASTNAME, employee.getLastName());
-					ResultSet result=preparedStatement.executeQuery();
+			  //fetch the employee educations from the database   
+				databaseEducations=educationDao.getEducations(connection, empId);   
 
-					while(result.next()) {                                                                 
-						empId=result.getInt("id");                                                          //fetch the id from the database and pass to the educationDao insert method
-						consoleOutput.displayInsertion(educationDao.insert(empId, employee.getEducation(),connection));	  //call the eduactionDao insert method in the employeeDao insert for inserting the educations in the database
-						
+				if(rowInserted>0) {  
+					//call the eduactionDao insert method in the employeeDao insert for inserting the educations in the database
+					consoleOutput.displayInsertion(educationDao.insert(empId, employee.getEducation(),connection,employee.getFirstName()));
 
-					}
+					update=false;
+
 
 				}
 
-			}	
-			
-			connection.commit();                                                                         //if all the  data inserted prope
-			connection.close();
+                //if  firstName and lastName are same in the file and if any other changes are found in the file then comes into the else if block 
+				else if(!databaseJob.equals(employee.getJob()) || !databaseCity.equals(employee.getCity()) || !databaseEducations.equals(employee.getEducation()) && update==true){ 
+				 
+					boolean flag=false;
+					int size=employee.getEducation().size();
+					int count=0;
+					if(count<=size) {
+
+						for(String education:employee.getEducation()) {
+							if(education.equals(databaseEducations.get(count))){
+								continue;
+							}else {
+								flag=true;
+							}
+							count++;
+						}
+					}
+					if(flag==true) {
+						//update method for update the  database data and if changes are found the file
+						consoleOutput.displayUpdate(update(employee.getCity(),employee.getJob(),employee.getFirstName(),employee.getLastName(),empId,connection,employee.getEducation()));	
+					}
+					
+                  //if no update in the file then comes into the else block
+				}else {
+					consoleOutput.displayUpToDate();
+				}
+
+
+			}
+
+
+			connection.commit();                                                                         //IF all the data are inserted in the  database then the data commit  else it will rollback the data
+
 		} 
 		catch (SQLException sqlException) {
 			try {
 
 				if(connection!=null) {
-			
+
 					connection.rollback();
 				}
 			}
 			catch (SQLException sqlException1) {
-				log.error("sqlException");
-				
+			sqlException1.printStackTrace();
+
 			}
+			sqlException.printStackTrace();
 			System.out.println("Data not inserted");
+		}finally {
+			if(connection!=null) {
+				try {
+					connection.close();
+					preparedStatement.close();
+				} catch (SQLException sqlException) {
+					sqlException.printStackTrace();
+				}
+			}
 		}
-	
+
+
 	}
+ 
+	/*
+	 * getEmployeeList method for fetch employee Data from database
+	 */
 
 
+	public  ArrayList<Employee> getEmployeeList(String FirstName,String LastName) {
+		ArrayList<Employee> dataBaseEmployeeList=new ArrayList<Employee>();
+		Employee employee = null;
+		int id = 0;
+		String job = null;
+		String city = null;
+		String firstName = null;
+		String lastName = null;
 
 
+		PreparedStatement preparedStatement=null;
 
-} 
+		String selectQuery="select * from employee_data where firstname=? and lastname=?";       //select query for fetch the record from the database
+		try {
+			preparedStatement=connection.prepareStatement(selectQuery);
+			preparedStatement.setString(FIRSTNAME,FirstName); 
+			preparedStatement.setString(LASTNAME, LastName);
+			ResultSet result=preparedStatement.executeQuery();
+			while(result.next()) {  
+				job=result.getString("job");
+				city=result.getString("city");
+				id=result.getInt("id");
+				firstName=result.getString("firstname");
+				lastName=result.getString("lastname"); 
+				employee=new Employee(id,firstName,lastName,city,job);
+
+
+			} 
+			dataBaseEmployeeList.add(employee);
+
+
+			return dataBaseEmployeeList;
+		} catch (SQLException sqlException) {
+			// TODO Auto-generated catch block
+			log.error("sql exception");
+		}
+		return null; 
+
+
+	}
+	
+	/*
+	 * 
+	 * update method for update the employee data in database
+	 */
+
+	public boolean update(String city,String job,String firstname,String lastname,int empId,Connection connection,ArrayList<String> educations) {
+		EducationDao educationDao=new EducationDao();
+		PreparedStatement preparedStatement=null;
+		String updateQuery="update employee_data set city=?,job=? where firstname =?and lastname=?";
+		try {
+			preparedStatement=connection.prepareStatement(updateQuery);
+			preparedStatement.setString(UPDATE_CITY,city);
+			preparedStatement.setString(UPDATE_JOB, job);
+			preparedStatement.setString(UPDATE_FIRST_NAME,firstname);
+			preparedStatement.setString(UPDATE_LAST_NAME, lastname);
+			int updateRow=preparedStatement.executeUpdate();
+			educationDao.update(empId,educations,connection,firstname);
+//			System.out.println("updated");
+			if(updateRow>0) {
+				return true;
+			}
+		} 
+
+		catch (SQLException sqlException) {
+			// TODO Auto-generated catch block
+			log.error("sql Exception");
+		}
+		return false;
+
+
+	}
+}	
+
+
 
 
 
