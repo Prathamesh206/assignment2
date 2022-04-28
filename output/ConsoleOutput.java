5b3be38639b5fa4aadca00b39b7c009c6910e040
Prathamesh206 <98940544+Prathamesh206@users.noreply.github.com>
2022-04-28 12:30:22 +0530

    Updated

--- output/ConsoleOutput.java
+++ output/ConsoleOutput.java
@@ -9,7 +9,7 @@ public class ConsoleOutput {
 	/*
 	 *  displayFilterJob Method Display Employee Details Filter by job
 	 */
-        int count=1;
+	int count=1;
 	public void displayFilterJobList(ArrayList<String> employeeList) {
 
 		for(String employee:employeeList) {                 //iterating each employee and display the full name of employee
@@ -17,36 +17,37 @@ public class ConsoleOutput {
 
 		}
 	}
-	
+
 
 
 	public void displayInsertion(boolean result) {
- 
-	
+
+
 		if(result)
 		{
-			
+
 			System.out.println( count++ + " "+"Insertion Successful");
 		}
 		else  {
 			System.out.println("Unsuccessful");
 		}
-		
+
 	}
-	
-	public void  displayUpdate(boolean employeeUpdate,boolean deleteUpdate) {
+
+	public void  displayUpdate(boolean employeeUpdate,boolean deleteUpdate,int empid) {
 		if(employeeUpdate) {
-			System.out.println(count++ + " "+"employee update succesfully");
+			System.out.println("employee_id= "+empid+ "  employee_table update succesfully");
 		}
 		else if(deleteUpdate) {
-			System.out.println(count++ + " "+"education update succesfully");
+			System.out.println("employee_id= "+empid+ "  education_table update succesfully");
 		}
 	}
-  
+
 	public void  displayUpToDate() {
-		
-			System.out.println("Data up to date");
-		
+
+		System.out.println("Data up to date");
+
 	}
 
 }
+
